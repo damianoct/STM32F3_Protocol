ad21c47cb86323954565ca76ca32b1871daadb75
Marco <marco.salafia@gmail.com>
2015-05-23 21:14:31 +0200

    Implementato il comando di echo

--- main.c
+++ main.c
@@ -6,6 +6,18 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 #define MAX_DIM 300
+#define MAX_HEADER_INDEX 4
+
+//Campi del pacchetto
+#define START 0
+#define CONF 1
+#define CODE 2
+#define PLEN_1 3
+#define PLEN_2 4
+
+//Codici dei comandi
+#define ECHO 0x00
+
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 __IO uint32_t TimingDelay = 0;
@@ -13,10 +25,14 @@ GPIO_InitTypeDef GPIO_InitStructure;
 
 /* Private function prototypes -----------------------------------------------*/
 void Delay(__IO uint32_t nTime);
-
 uint16_t readPayloadLength(uint8_t firstLen, uint8_t secondLen);
+uint8_t isBitSet(uint8_t buffer, int index);
 
-uint8_t buffer[MAX_DIM], pacchetto[MAX_DIM];
+void BAU_PacketClone(uint8_t buffer[]);
+void BAU_PacketDispatch(uint8_t pack[]);
+void BAU_Echo(uint8_t pack[]);
+
+uint8_t buffer[MAX_DIM], packet[MAX_DIM];
 uint16_t pLen;
 int readProgress = 0;
 
@@ -108,22 +124,22 @@ void USART2_IRQHandler(void)
             if(USART_ReceiveData(USART2) == 0x2E)
             {
                 buffer[readProgress] = USART_ReceiveData(USART2);
-                USART_SendData(USART2,buffer[readProgress]);
+                //USART_SendData(USART2,buffer[readProgress]);
                 readProgress++;
             }
             //Altrimenti non fa niente
         }
         //Sto assemblando l'header
-        else if (readProgress < 6)
+        else if (readProgress < MAX_HEADER_INDEX + 1) //Fine dell'HEADER all'indice MAX_HEADER
         {
             buffer[readProgress] = USART_ReceiveData(USART2);
-            USART_SendData(USART2,buffer[readProgress]);
+            //USART_SendData(USART2,buffer[readProgress]);
             readProgress++;
-            if(readProgress == 6)
+            if(readProgress == MAX_HEADER_INDEX + 1) //Se abbiamo finito di assemblare l'Header
             {
-                pLen = readPayloadLength(buffer[4], buffer[5]);
+                pLen = readPayloadLength(buffer[PLEN_1], buffer[PLEN_2]);
                 //Se il payload è troppo grande lo considera sbagliato e scarta il pacchetto
-                if (pLen > MAX_DIM - 8)
+                if (pLen > MAX_DIM - 7)
                 {
                     readProgress = 0;
                     memset(buffer, 0, MAX_DIM);
@@ -131,23 +147,24 @@ void USART2_IRQHandler(void)
             }
         }
         //Sto assemblando il Payload
-        else if (readProgress < 6 + pLen)
+        else if (readProgress < MAX_HEADER_INDEX + 1 + pLen)
         {
             buffer[readProgress] = USART_ReceiveData(USART2);
-            USART_SendData(USART2,buffer[readProgress]);
+            //USART_SendData(USART2,buffer[readProgress]);
             readProgress++;
         }
         //Sto assemblando il CRC
         else if (readProgress < MAX_DIM)
         {
             buffer[readProgress] = USART_ReceiveData(USART2);
-            USART_SendData(USART2,buffer[readProgress]);
+            //USART_SendData(USART2,buffer[readProgress]);
             readProgress++;
-            if(readProgress == 8 + pLen)
+            if(readProgress == 7 + pLen)
             {
                 readProgress = 0;
-                //ElaboraPacchetto()
-                //memset(buffer, 0, MAX_DIM);
+                BAU_PacketClone(buffer);
+                BAU_PacketDispatch(packet);
+                memset(buffer, 0, MAX_DIM);
             }
         }
     }
@@ -156,15 +173,68 @@ void USART2_IRQHandler(void)
 
 uint16_t readPayloadLength(uint8_t firstLen, uint8_t secondLen)
 {
-  uint16_t tmpF, tmpS;
+    uint16_t tmpF, tmpS;
   
-  tmpF = (uint16_t) firstLen;
-  tmpF <<= 8;
-  tmpS = (uint16_t) secondLen;
+    tmpF = (uint16_t) firstLen;
+    tmpF <<= 8;
+    tmpS = (uint16_t) secondLen;
   
-  return tmpF | tmpS;
-  
+    return tmpF | tmpS;
 }
+
+uint8_t isBitSet(uint8_t buffer, int index)
+{
+    uint8_t mask = (1 << index);
+    return buffer & mask;
+}
+
+//Ritorna void perchè uso packet che è globale; Non servirebbe nemmeno il parametro.
+void BAU_PacketClone(uint8_t buffer[])
+{
+    int i, len = 0;
+    memset(packet, 0, MAX_DIM);
+    len = readPayloadLength(buffer[PLEN_1], buffer[PLEN_2]);
+    //Esiste in alternativa la funzione memcpy
+    for(i=0; i < len + 7; i++)
+    {
+        packet[i] = buffer[i];
+    }    
+}
+
+void BAU_PacketSend(uint8_t pack[])
+{
+    int i=0, len = 0;
+    len = readPayloadLength(pack[PLEN_1], pack[PLEN_2]);
+    for(i=0; i < len + 7; i++)
+    {
+        while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+            USART_SendData(USART2, pack[i]);
+    }
+}
+
+void BAU_Echo(uint8_t pack[])
+{
+    BAU_PacketSend(pack);
+}
+
+void BAU_PacketDispatch(uint8_t pack[])
+{
+    //Controllo l'integrità del pacchetto
+    if(isBitSet(pack[1], 4)) //Non ricordo un altro metodo per farlo
+    {
+        //Qui va il codice per controllare se il CRC corrisponde a quello
+        //calcolato. Se non corrisponde si scarta il pacchetto con un return.
+    }
+    
+    if(!isBitSet(pack[1], 6)) //Controllo che il pacchetto sia solo di request.
+            return;
+    
+    //Cambiarlo con uno switch case sul code sarebbe meglio?
+    if(pack[CODE] == ECHO)
+        BAU_Echo(pack);
+        
+}
+
 
 
 #ifdef  USE_FULL_ASSERT
